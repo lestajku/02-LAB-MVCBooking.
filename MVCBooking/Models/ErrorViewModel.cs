36445f5fe65cfdd607e4192388ef2a22c2ca4aab
Paolo Stabile <pastabil@vsite.hr>
2025-11-04 16:50:50 +0100

    Add project files.

---
+++ MVCBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
