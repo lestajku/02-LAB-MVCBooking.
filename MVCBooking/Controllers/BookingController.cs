d28abda27cc7121da199721537d9937724bfcf81
lestajku <lestajku@vsite.hr>
2026-01-20 19:38:15 +0100

    Labos 2 - Leonora Stajku

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+        private static int nextId = 1;
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateBooking(HotelBooking newBooking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", newBooking);
+            }
+            newBooking.Id = nextId++;
+            bookings.Add(newBooking);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
